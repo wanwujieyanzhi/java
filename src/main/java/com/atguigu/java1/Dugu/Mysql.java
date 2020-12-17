181d8db58ea8d3069103496bc379e281e1db3ee3
wanwujieyanzhi <1240821552@qq.com>
2020-12-17 14:57:13 +0800

    Initial commit

---
+++ src/main/java/com/atguigu/java1/Dugu/Mysql.java
@@ -0,0 +1,9 @@
+package com.atguigu.java1.Dugu;
+
+public class Mysql {
+    public static void main(String[] args) {
+        System.out.println("nihao");
+        System.out.println("nihao");
+        System.out.println("nihao");
+    }
+}
